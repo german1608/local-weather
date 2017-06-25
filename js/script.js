c4232c83e85180c3025d80e11c7e267065dc256f
German Robayo Paz <germanrobayo33@gmail.com>
2017-06-25 15:34:44 -0400

    Fix the ajax request

--- js/script.js
+++ js/script.js
@@ -1,19 +1,40 @@
+function resizePage() {
+  // Helps center the content vertically
+  $('#entire-page').height($(window).height());
+}
+
 $(document).ready(function () {
+  resizePage();
+  $(window).on('resize', resizePage);
+
   navigator.geolocation.getCurrentPosition(
   function (position) {
     var long = position.coords.longitude,
         lat = position.coords.latitude,
+    // This API will get the weather
+    urlWeather = "https://api.darksky.net/forecast/1974cc7f739ce40da31733af089ed026/" + lat + "," + long,
     // This API will get the current location
-    posAPI = "https://api.darksky.net/forecast/1974cc7f739ce40da31733af089ed026/" + lat + "," + long;
+    urlPos = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + long + "&sensor=true";
+    let currentWeather = "", user_location = "";
     $.ajax({
-        url: posAPI,
-        dataType: 'jsonp',
+      url: urlWeather,
+      dataType: 'jsonp',
     })
     .done(function(json) {
-        console.log(json);
-    })
-    .fail(function() {
-        console.log("error");
+      currentWeather = json.currently;
+      // console.log(currentWeather);
+      $.ajax({
+        url: urlPos,
+        dataType: 'json',
+      })
+      .done(function(json) {
+        user_location = json.results[6].formatted_address;
+        $('#entire-page').html(`${currentWeather}, ${user_location}`);
+      })
+      .fail(function() {
+        user_location = "your location";
+      });
     });
+    console.log(user_location, currentWeather);
   });
 });
