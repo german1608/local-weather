e0e6b113f47951d796502611f2d4a7f028d5d342
German Robayo Paz <germanrobayo33@gmail.com>
2017-06-24 23:43:26 -0400

    testing around

--- js/script.js
+++ js/script.js
@@ -1,24 +1,27 @@
 $(document).ready(function () {
   navigator.geolocation.getCurrentPosition(
-    function (position) {
+  function (position) {
     var long = position.coords.longitude,
         lat = position.coords.latitude,
     // This API will get the current location
     posAPI = "https://api.darksky.net/forecast/1974cc7f739ce40da31733af089ed026/" + lat + "," + long;
-    $.getJSON(posAPI, function(json, textStatus) {
-        console.log(json);
-    });
-    //     posAPI = 
-    // "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + long + "&sensor=true";
-    // $.getJSON(posAPI, function(json, textStatus) {
-    //     console.log(json.results[6]);
-    //     var location = json.results[6];
-    //     var country = location.address_components[1].short_name,
-    //       state = location.address_components[0].short_name,
-    //         weatherAPI = 
-    //     "http://api.wunderground.com/api/72d4e41a0b24a84b/conditions/q/" + country + "/THE_DESIRED_CITY.json"
-    //     console.log(country, state);
+    console.log(posAPI);
+    // $.ajax({
+    //   url: posAPI,
+    //   dataType: 'json',
+    //   success: function (json) {
+    //     console.log(json);
+    //   }
+    // })
+    // .done(function() {
+    //   console.log("success");
+    // })
+    // .fail(function() {
+    //   console.log("error");
+    // })
+    // .always(function() {
+    //   console.log("complete");
     // });
+    
   });
-  // Made in ajax_request branch
 });
