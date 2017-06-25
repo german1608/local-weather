1507d634d38989ab74661c576a3ee4f7a58edf50
German Robayo Paz <germanrobayo33@gmail.com>
2017-06-25 13:00:49 -0400

    Testing

--- js/script.js
+++ js/script.js
@@ -5,23 +5,15 @@ $(document).ready(function () {
         lat = position.coords.latitude,
     // This API will get the current location
     posAPI = "https://api.darksky.net/forecast/1974cc7f739ce40da31733af089ed026/" + lat + "," + long;
-    console.log(posAPI);
-    // $.ajax({
-    //   url: posAPI,
-    //   dataType: 'json',
-    //   success: function (json) {
-    //     console.log(json);
-    //   }
-    // })
-    // .done(function() {
-    //   console.log("success");
-    // })
-    // .fail(function() {
-    //   console.log("error");
-    // })
-    // .always(function() {
-    //   console.log("complete");
-    // });
-    
+    $.ajax({
+        url: posAPI,
+        dataType: 'jsonp',
+    })
+    .done(function(json) {
+        console.log(json);
+    })
+    .fail(function() {
+        console.log("error");
+    });
   });
 });
