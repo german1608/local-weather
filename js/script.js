ef9712d4d4a2112742127c41a403943156050d8d
German Robayo <germanrobayo33@gmail.com>
2017-06-26 17:02:21 -0400

    Update script.js
    Almost finished

--- js/script.js
+++ js/script.js
@@ -1,15 +1,29 @@
+var celsius = true;
+
 function resizePage() {
   // Helps center the content vertically
   $('#entire-page').height($(window).height());
 }
 
 function convertCtoF(celsius) {
-  return Math.round((celsius * 9 / 5 + 32) * 100) / 100;
+  return roundTo2((celsius * 9 / 5 + 32) * 100);
 }
 
 function convertFtoC(farenheit) {
-  return Math.round(((farenheit - 32) * 5 / 9) * 100) / 100;
+  return roundTo2((farenheit - 32) * 5 / 9);
 }
+
+function roundTo2(n) {
+  return Math.round(n * 100) / 100;
+}
+
+/*function toggleGrades() {
+  if (celsius) {
+    celsius = false;
+    var cur = parseInt($('#temperature').html());
+  }
+}*/
+
 
 $(document).ready(function () {
   resizePage();
@@ -22,13 +36,12 @@ $(document).ready(function () {
     urlWeather = "https://api.darksky.net/forecast/1974cc7f739ce40da31733af089ed026/" + lat + "," + long,
     // This API will get the current location
     urlPos = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + long + "&sensor=true";
-    let currentWeather = "", user_location = "";
+    let currWeather = "", user_location = "";
     $.ajax({
       url: urlWeather,
       dataType: 'jsonp',
     })
     .done(function(json) {
-
       // Guardamos la info del clima
       /* Data que me interesa de json.currently:
         * icon: [
@@ -49,23 +62,32 @@ $(document).ready(function () {
         * temperature
         * windSpeed
       */
-      currentWeather = json.currently;
+      currWeather = json.currently;
       console.log(json);
+      $('#icon').html(`
+        <img src="icons/${currWeather.icon}.png" alt="${currWeather.icon}" class="responsive-img"/>`);
+      $('#summary').text(currWeather.summary);
+      $('#wind-speed').text(roundTo2(currWeather.windSpeed * 1.6) + " km/h");
+      $('#humidity').text(roundTo2(currWeather.humidity));
+      $('#pressure').text(roundTo2(currWeather.pressure) + " mbar");
       // console.table(currentWeather);
       // console.log(convertFtoC(currentWeather));
       // Hacemos la llamada al api de google para obtener la ubicacion actual
-      $.ajax({
-        url: urlPos,
-        dataType: 'json',
-      })
-      .done(function(json) {
-        user_location = json.results[6].formatted_address;
-        
-      })
-      .fail(function() {
-        user_location = "your location";
-      });
+    });
+
+    $.ajax({
+      url: urlPos,
+      dataType: 'json',
+    })
+    .done(function(json) {
+      user_location = json.results[5].address_components[0].short_name;
+      console.log(user_location);
+      $('#title').text(user_location);
+    })
+    .fail(function() {
+      user_location = "your location";
     });
   });
+  $('#box').addClass('visible');
   $(window).on('resize', resizePage);
 });
